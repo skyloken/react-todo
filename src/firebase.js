2b55df101ae23cc6e6ab22ad01e6bdbe6aedc1e3
Kenta Tanigaki <risora1234@gmail.com>
2019-08-03 18:21:39 +0900

    Firebaseをセットアップ

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAHga1nEhGIYah_KVCCy33-jIZAWXFEbuQ",
+    authDomain: "react-todo-d75a3.firebaseapp.com",
+    databaseURL: "https://react-todo-d75a3.firebaseio.com",
+    projectId: "react-todo-d75a3",
+    storageBucket: "",
+    messagingSenderId: "536674971008",
+    appId: "1:536674971008:web:21d41d00f3f7d8a0"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const firestore = firebase.firestore();
+export const auth = firebase.auth();
